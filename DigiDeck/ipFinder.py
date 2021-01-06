0cf7ac76ac6049f1a07dbda104a0474922d53043
sockheadrps <39883642+sockheadrps@users.noreply.github.com>
2021-01-05 23:40:05 -0500

    Update ipFinder.py

--- DigiDeck/ipFinder.py
+++ DigiDeck/ipFinder.py
@@ -1,6 +1,32 @@
 import socket
+import os
+
 hostname = socket.gethostname()
 IPAddr = socket.gethostbyname(hostname)
+
+file = open('xhr.js', 'w+')
+file.write('let btns = document.getElementsByClassName("button");')
+file.write('\n')
+file.write('\nfor (var i=0; i < btns.length; i++){')
+file.write('\n\tlet btn = btns[i]')
+file.write("\n\tlet func = () => {")
+file.write('\n\t\tlet xhr = new XMLHttpRequest();')
+file.write('\n\t\txhr.open("GET", ' + f"`http://{IPAddr}:8000" + "/${btn.id}`)")
+file.write('\n\t\txhr.onload = () => {')
+file.write('\n\t\t\tif(xhr.status == 200)')
+file.write('\n\t\t\t\tconsole.log(xhr.responseText);')
+file.write('\n\t\t\telse')
+file.write('\n\t\t\t\tconsole.log(xhr.statusText);')
+file.write('\n\t\t}')
+file.write('\n\t\txhr.send();')
+
+
+
+file.write("\n\t}")
+file.write("\n\tbtns[i].addEventListener('click', func);")
+file.write('\n}')
+
+
 print("#############")
 print("#############")
 print("#############")
@@ -13,3 +39,4 @@ print("#############")
 print("#############")
 print("#############")
 print("#############")
+
